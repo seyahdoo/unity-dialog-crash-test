5a12dcb4a91398eeab34b2bbeb5518dca3d20125
seyyid ahmed doğan <seyahdoo@gmail.com>
2021-04-09 13:21:50 +0300

    add project

---
+++ Assets/Editor/CashTesterEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CrashTester))]
+public class CashTesterEditor : Editor {
+    public void OnSceneGUI() {
+        if (Event.current.isKey) {
+            if (EditorUtility.DisplayDialog("title", "message", "ok", "cancel")) {
+                Debug.Log("ok");
+            }
+            else {
+                Debug.Log("cancel");
+            }
+        }
+    }
+}
